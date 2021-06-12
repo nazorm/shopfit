0308699dab52fd03f0b02323038d498114e628fe
nazorm <nnadoziechinazor@gmail.com>
2021-06-12 21:18:17 +0100

    set route for paths

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from "./Header";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Filter from './components/filter/components/Filter';
 import Home from './components/home/components/Home';
 import Cart from './components/cart/components/Cart';
 import "./App.scss";
@@ -9,10 +10,11 @@ const App: React.FC = () => {
   return (
     <Router>
       <div className="container">
+      <Header />
+      <Filter/>
         <Switch>
-          <Header />
           <Route exact path ='/'><Home/></Route>
-          <Route exact path='/cart'><Cart/></Route>
+          <Route exact path ='/cart'><Cart/></Route>
         </Switch>
       </div>
     </Router>
