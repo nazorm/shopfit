dc9ce42400f5aaa0d1f4e92c04e826b8b8d80493
nazorm <nnadoziechinazor@gmail.com>
2021-06-12 21:18:59 +0100

    clean up

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,12 +3,11 @@
 function Header():JSX.Element{
   return (
     <header className="header">
-      <h1>Shopfit</h1>
+      <h3>Shopfit</h3>
       <nav className="header-nav">
         <img src="" alt="hamburger" className="hamburger" />
         <ul className="nav-ul">
-          <li className="nav-li">Welcome newbie</li>
-          <li className="nav-li">Cart</li>
+          <li className="nav-li">Welcome-newbie</li>
         </ul>
       </nav>
     </header>
