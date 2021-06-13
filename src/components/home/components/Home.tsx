bc53e9cbefaaa774d3ecf1083c486c4727f5bce1
nazorm <nnadoziechinazor@gmail.com>
2021-06-13 22:27:45 +0100

    Product card

--- src/components/home/components/Home.tsx
+++ src/components/home/components/Home.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import './scss/styles.scss'
+import { IProductCardProps } from "./type";
 
-const Home: React.FC = () => {
+
+const Home= ({productcard}: IProductCardProps) => {
+const {id, title, price, description, image} = productcard
   return (
-    <div className="home-container">
-      <Link to="/cart">go to cart</Link>
-      <div>
-        
+    <div className="product-card">
+      {/* <Link to="/cart">go to cart</Link> */}
+      <div key={id}>
+        <img src={image} className = 'product-image' alt='product' height='200px'/>
+        <h4 className='product-name'>{title}</h4>
+        <p className='description'>{description}</p>
+        <h4 className='product-price'>${" "}{price}</h4>
+        <button className='addtocart-btn'>Add to Cart</button>
       </div>
     </div>
   );
