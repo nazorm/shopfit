eb9c43c23eae0ece15203be2dfe067cdd821aa26
nazorm <nnadoziechinazor@gmail.com>
2021-06-14 19:12:27 +0100

    cart

--- src/components/cart/components/Cart.tsx
+++ src/components/cart/components/Cart.tsx
@@ -1,14 +1,42 @@
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
+import "../../home/components/scss/styles.scss";
+import { ICartProps } from "./types";
 
+function Cart({ cartProducts, handleRemovefromCart }: ICartProps): JSX.Element {
+  const style = {
+    backgroundColor: "red",
+    color: "white",
+  };
 
-function Cart():JSX.Element{
-    return(
-        <div>
-            <h1>This is Cart</h1>
-            <Link to='/'>Back Home</Link>
-
+  const { id, title, price, description, image } = cartProducts;
+  if (!id) {
+    return <p></p>;
+  }
+  return (
+    <div className="product-card">
+      <Link to="/">go to home</Link>
+      <div key={id}>
+        <img
+          src={image}
+          className="product-image"
+          alt="product"
+          height="200px"
+        />
+        <h4 className="product-name">{title}</h4>
+        <div className="description-container">
+          <p className="description">{description}</p>
         </div>
-    )
+        <h4 className="product-price">$ {price}</h4>
+        <button
+          className="handlecart-btn"
+          onClick={() => handleRemovefromCart(id)}
+          style={style}
+        >
+          Remove
+        </button>
+      </div>
+    </div>
+  );
 }
 
-export default Cart
+export default Cart;
