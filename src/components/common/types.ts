6b03662a00deffd0e7ae9091779a892f3194c6ad
nazorm <nnadoziechinazor@gmail.com>
2021-06-14 19:12:50 +0100

    clean up

--- src/components/common/types.ts
+++ src/components/common/types.ts
@@ -1,8 +1,8 @@
 export interface IProduct {
-  id?: number;
-  title?: string;
-  price?: string;
-  description?: string;
-  image?: string;
+  id: number;
+  title: string;
+  price: string;
+  description: string;
+  image: string;
   
 }
