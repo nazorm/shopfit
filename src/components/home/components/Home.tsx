9cdd0bc2a0f15854502aa0d5ca3efc672a4ed04c
nazorm <nnadoziechinazor@gmail.com>
2021-06-14 19:12:08 +0100

    clean up

--- src/components/home/components/Home.tsx
+++ src/components/home/components/Home.tsx
@@ -1,20 +1,32 @@
 import React from "react";
-// import { Link } from "react-router-dom";
-import './scss/styles.scss'
+import { Link } from "react-router-dom";
+import "./scss/styles.scss";
 import { IProductCardProps } from "./type";
 
-
-const Home= ({productcard}: IProductCardProps) => {
-const {id, title, price, description, image} = productcard
+const Home = ({ productcard, handleCart }: IProductCardProps) => {
+  const { id, title, price, description, image } = productcard;
+  if (!id) {
+    return <p></p>;
+  }
   return (
     <div className="product-card">
-      {/* <Link to="/cart">go to cart</Link> */}
+      <Link to="/cart">go to cart</Link>
       <div key={id}>
-        <img src={image} className = 'product-image' alt='product' height='200px'/>
-        <h4 className='product-name'>{title}</h4>
-        <p className='description'>{description}</p>
-        <h4 className='product-price'>${" "}{price}</h4>
-        <button className='addtocart-btn'>Add to Cart</button>
+        <img
+          src={image}
+          className="product-image"
+          alt="product"
+          height="200px"
+        />
+        <h4 className="product-name">{title}</h4>
+        <div className="description-container">
+          <p className="description">{description}</p>
+        </div>
+
+        <h4 className="product-price">$ {price}</h4>
+        <button className="handlecart-btn" onClick={() => handleCart(id)}>
+          Add to Cart
+        </button>
       </div>
     </div>
   );
