73b3fcf84c45efe818c722c33a7f78609b397102
nazorm <nnadoziechinazor@gmail.com>
2021-06-15 01:53:24 +0100

    handle filters

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,20 @@ const App = (): JSX.Element => {
   const [loading, setLoading] = useState<boolean>(true);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [propductsPerPage] = useState<number>(9);
+  const [searchValue, setSearchValue] = useState<string>("");
+  const [filteredProducts, setFilteredProducts] = useState<IProduct[]>([]);
+  const [filters, setFilters] = useState({
+    search: "",
+    category: "",
+    date: "",
+  });
 
   const fetchProducts = async () => {
     const res = await axios.get("https://fakestoreapi.com/products");
     setProducts(res.data);
+    setFilteredProducts(res.data);
     setLoading(false);
   };
-
-  // get current posts
-  const indexOfLastProduct = currentPage * propductsPerPage;
-  const indexOfFirstProduct = indexOfLastProduct - propductsPerPage;
-  const currentList = products?.slice(indexOfFirstProduct, indexOfLastProduct);
 
   // handle page
   const handlePage = (number: number) => {
@@ -44,23 +47,119 @@ const App = (): JSX.Element => {
     const cartItems = [cartProduct, ...cart];
     setCart(cartItems);
   };
+  // handle filters
+  const handleFilters = (newFilter: {
+    category: string;
+    search: string;
+    date: string;
+    order?: string;
+  }) => {
+    let isFilteredProducts = products;
+
+    //default
+    // eslint-disable-next-line eqeqeq
+    if (newFilter.order == "default" && newFilter.category == "default") {
+      isFilteredProducts = products;
+      setFilteredProducts(isFilteredProducts);
+      return;
+    }
+    //order
+    if (newFilter.order) {
+      switch (newFilter.order) {
+        case "low":
+          isFilteredProducts = isFilteredProducts.sort(
+            (a, b) => +a.price - +b.price
+          );
+          break;
+        case "high":
+          isFilteredProducts = isFilteredProducts.sort(
+            (a, b) => Number(b.price) - Number(a.price)
+          );
+          break;
+        default:
+          isFilteredProducts = products;
+          break;
+      }
+    }
+
+    // category
+    if (newFilter.category) {
+      isFilteredProducts = isFilteredProducts.filter((product) => {
+        return product.title.includes(newFilter.category);
+      });
+    }
+
+    //search
+    if (newFilter.search) {
+      isFilteredProducts = isFilteredProducts.filter((product) => {
+        return `${product.title} ${product.price} ${product.description}`
+          .toLowerCase()
+          .includes(newFilter.search);
+      });
+    }
+    setFilteredProducts(isFilteredProducts);
+  };
+
+  // handle category change
+  const handleCategoryChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    console.log(value);
+    const newFilter = { ...filters, category: value };
+    setFilters(newFilter);
+    handleFilters(newFilter);
+  };
+
+  // handle order change
+  const handleOrderChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    const newFilter = { ...filters, order: value };
+    setFilters(newFilter);
+    handleFilters(newFilter);
+  };
+
+  //handle search change
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    const value = e.target.value;
+    setSearchValue(value);
+  };
+
+  // handle search
+  const handleSearch = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const newFilter = { ...filters, search: searchValue };
+    setFilters(newFilter);
+    handleFilters(newFilter);
+  };
 
   // handle remove from cart
   const handleRemovefromCart = (id: number) => {
-   const filteredCartProduct = cart.filter((product)=>{
-     return product.id !== id
-   });
-   setCart(filteredCartProduct)
+    const filteredCartProduct = cart.filter((product) => {
+      return product.id !== id;
+    });
+    setCart(filteredCartProduct);
   };
+  // get current posts
+  const indexOfLastProduct = currentPage * propductsPerPage;
+  const indexOfFirstProduct = indexOfLastProduct - propductsPerPage;
+  const currentList = filteredProducts?.slice(
+    indexOfFirstProduct,
+    indexOfLastProduct
+  );
   useEffect(() => {
     fetchProducts();
   }, []);
-  
+
   return (
     <Router>
       <div className="container">
         <Header />
-        <Filter />
+        <Filter
+          handleSearch={handleSearch}
+          handleSearchChange={handleSearchChange}
+          handleCategoryChange={handleCategoryChange}
+          handleOrderChange={handleOrderChange}
+        />
         <Switch>
           <Route exact path="/">
             {loading || !currentList ? (
@@ -81,7 +180,7 @@ const App = (): JSX.Element => {
           </Route>
           <Route exact path="/cart">
             {cart.length === 0 ? (
-              <h2 className='emptycart-text'>
+              <h2 className="emptycart-text">
                 Nothing in Cart <Link to="/">Start Shopping</Link>
               </h2>
             ) : (
