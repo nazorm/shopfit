ee30109697d177b371d5564b0ab529746b21088d
nazorm <nnadoziechinazor@gmail.com>
2021-06-21 00:00:48 +0100

    landing screen

--- src/components/landing/components/Landing.tsx
+++ src/components/landing/components/Landing.tsx
@@ -2,45 +2,55 @@ import React from "react";
 import { ICartegoryImagesProps } from "./types";
 import { categories } from "./categories";
 import {Link} from 'react-router-dom'
+import './styles.scss'
 
 
 const Landing = (): JSX.Element => {
   const { hats, kiddies, men, turtleneck, vintage, women } = categories;
 
   return (
-      <React.Fragment>
-    <section className="landing-container">
+      <section className='landing-container'>
+    <section className="landingcategories-container">
       <div className="category">
         <img src={women} className="category-image" alt="category" height='150px' />
-        <button className='btn btn--category'>Shop</button>
+        <label className='landing-label'>Women</label>
+        <button className='btn btn--category'>Shop Now</button>
       </div>
       <div className="category">
         <img src={men} className="category-image" alt="category" height='150px' />
-        <button className='btn btn--category'>Shop</button>
+        <label className='landing-label'>Men</label>
+        <button className='btn btn--category'>Shop Now</button>
       </div>
       <div className="category">
         <img src={kiddies} className="category-image" alt="category" height='150px' />
-        <button className='btn btn--category'>Shop</button>
+        <label className='landing-label'>Kiddies</label>
+        <button className='btn btn--category'>Shop Now</button>
       </div>
       <div className="category">
         <img src={hats} className="category-image" alt="category" height='150px' />
-        <button className='btn btn--category'>Shop</button>
+        <label className='landing-label'>Hats</label>
+        <button className='btn btn--category'>Shop Now</button>
       </div>
+     
       <div className="category">
         <img src={turtleneck} className="category-image" alt="category" height='150px' />
-        <button className='btn btn--category'>Shop</button>
+        <label className='landing-label'>Turtle Neck</label>
+        <button className='btn btn--category'>Shop Now</button>
       </div>
       <div className="category">
         <img src={vintage} className="category-image" alt="category" height='150px' />
-        <button className='btn btn--category'>Shop</button>
+        <label className='landing-label'>Vintage</label>
+        <button className='btn btn--category'>Shop Now</button>
       </div>
     </section>
-    <button className='btn'>
-        <Link to='/home'>
-            Shop
-        </Link>
+    <Link to='/home' className='btn-link'>
+    <button className='btn btn--link'>
+    
+            Shop Now
+        
     </button>
-    </React.Fragment>
+    </Link>
+    </section>
   );
 };
 
