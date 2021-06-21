2765f0a505e4848336f1797f3d32bdf770c44fba
nazorm <nnadoziechinazor@gmail.com>
2021-06-21 02:56:39 +0100

    clean up

--- src/components/cart/components/Cart.tsx
+++ src/components/cart/components/Cart.tsx
@@ -1,43 +1,42 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import '../../home/components/styles.scss';
+import "../../home/components/styles.scss";
 import { ICartProps } from "./types";
 
 function Cart({ cartProducts, handleRemovefromCart }: ICartProps): JSX.Element {
+  const { id, title, price, description, image } = cartProducts;
+
   const style = {
     backgroundColor: "red",
     color: "white",
   };
 
-  const { id, title, price, description, image } = cartProducts;
   if (!id) {
     return <p></p>;
   }
   return (
-      <React.Fragment>     
-    <div className="product-card">
-    <Link to="/home">go to home</Link>
-      <div key={id}>
-        <img
-          src={image}
-          className="product-image"
-          alt="product"
-          height="200px"
-        />
-        <h4 className="product-name">{title}</h4>
-        <div className="description-container">
-          <p className="description">{description}</p>
+    <React.Fragment>
+      <div className="product-card">
+        <div key={id}>
+          <img
+            src={image}
+            className="product-image"
+            alt="product"
+            height="200px"
+          />
+          <h4 className="product-name">{title}</h4>
+          <div className="description-container">
+            <p className="description">{description}</p>
+          </div>
+          <h4 className="product-price">$ {price}</h4>
+          <button
+            className="handlecart-btn"
+            onClick={() => handleRemovefromCart(id)}
+            style={style}
+          >
+            Remove
+          </button>
         </div>
-        <h4 className="product-price">$ {price}</h4>
-        <button
-          className="handlecart-btn"
-          onClick={() => handleRemovefromCart(id)}
-          style={style}
-        >
-          Remove
-        </button>
       </div>
-    </div>
     </React.Fragment>
   );
 }
