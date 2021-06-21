0ab3f87b612a747ac3a3424fc4e87b1d308d89ba
nazorm <nnadoziechinazor@gmail.com>
2021-06-21 02:56:27 +0100

    header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,17 +1,33 @@
-
-
-function Header():JSX.Element{
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Link } from "react-router-dom";
+import { ICartCounter } from "./components/common/types";
+function Header({cartCounter} : ICartCounter): JSX.Element {
   return (
     <header className="header">
-      <h3>Shopfit</h3>
+      <h3 className="company-name">Shopfit</h3>
       <nav className="header-nav">
-        <img src="" alt="hamburger" className="hamburger" />
-        <ul className="nav-ul">
-          <li className="nav-li">Welcome Shopper</li>
-        </ul>
+      <Link to='/'>Home</Link>
+        <a
+          href="http://placecorgi.com"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Welcome Shopper
+        </a>
+        <a
+          href="http://placecorgi.com"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          Sign In
+        </a>
+        <Link to="/cart" className="to-cart">
+          <ShoppingCartOutlined className="hamburger" />
+          <span className="cart-counter">{cartCounter}</span>
+        </Link>
       </nav>
     </header>
   );
-};
+}
 
 export default Header;
